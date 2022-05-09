155aea67ce45a5313604886dacd7f1d2278ef7bd
Caroline Moraes da Cruz <carolmoraescruz@gmail.com>
2022-05-09 16:38:38 -0300

    imp: Notas de aula 220506

---
+++ src/main/java/br/com/hello/CadastroServlet.java
@@ -0,0 +1,24 @@
+package br.com.hello;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/cadastro")
+public class CadastroServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	String nome = request.getParameter("nome");
+    	PrintWriter out = response.getWriter();
+    	
+    	out.println("<html><body>Nome: " + nome + " cadastrado(a) com sucesso!</body></html>");
+    	
+    }
+
+}
